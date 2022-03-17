fcd43350de20dbd0bc5abb2b3e3ac3b6a7851ad7
N1ckSL <savilencu.n@gmail.com>
2022-03-17 21:57:30 +0200

    first setup

---
+++ pages/index.js
@@ -0,0 +1,322 @@
+import NextLink from 'next/link'
+import {
+  Link,
+  Container,
+  Heading,
+  Box,
+  SimpleGrid,
+  Button,
+  List,
+  ListItem,
+  useColorModeValue,
+  chakra,
+  Tooltip,
+  Center,
+  Badge
+} from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import Paragraph from '../components/paragraph'
+import { BioSection, BioYear } from '../components/bio'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import { GridItem } from '../components/grid-item'
+import {
+  IoLogoLinkedin,
+  IoLogoInstagram,
+  IoLogoGithub,
+  IoLogoDiscord,
+  IoLogoSteam
+} from 'react-icons/io5'
+import thumbMogo from '../public/images/works/mogo.png'
+import thumbReact from '../public/images/works/mock.png'
+import Image from 'next/image'
+import { Meta } from '../components/work'
+
+const ProfileImage = chakra(Image, {
+  shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
+})
+
+const Home = () => (
+  <Layout>
+    <Container maxW="container.lg">
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+      >
+        ✨ Hello, I&apos;m a junior web developer ✨
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Savilencu Nicolae
+          </Heading>
+          <p>
+            Digital creator ( <Badge colorScheme="red"> Student</Badge> /{' '}
+            <Badge colorScheme="purple"> Developer</Badge> /{' '}
+            <Badge colorScheme="cyan"> Sole of company </Badge> )
+          </p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <ProfileImage
+              src="/images/me.jpg"
+              alt="Profile image"
+              borderRadius="full"
+              width="100%"
+              height="100%"
+            />
+          </Box>
+        </Box>
+      </Box>
+
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          About me
+        </Heading>
+        <Paragraph>
+          I am <span style={{ color: 'red' }}> 22 </span>/💯 years old. I was
+          born in Republic of Moldova, Cahul. In 2017 I started my road to
+          getting involved in dev. My first steps were creating basic static web
+          pages in notebook and then changing it's extension to{' '}
+          <em style={{ color: '#81E6D9' }}> .html </em>. Although those were
+          very dorky, I felt empowered by the fact that I could do something by
+          myself and see the results on the screen. It was something I really
+          found myself passionate about and I started getting more deep into
+          investigating.
+          <br />
+          <br />
+          Most exciting thing for me is looking back to where I was at the
+          begining and comparing my achievements, successes, and failures. This
+          gives me motivation to continuously evolve. Looking back to where I
+          started - it's a long road, which I do not regreting taking.
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works" scroll={false}>
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection mb={4}>
+          <BioYear>
+            August <span style={{ color: '#81E6D9' }}> 1999 </span>{' '}
+          </BioYear>
+          <br />
+          Born in Cahul, Republic of Moldova.
+        </BioSection>
+        <BioSection mb={4}>
+          <BioYear>
+            July <span style={{ color: '#81E6D9' }}> 2021 </span>
+          </BioYear>
+          <br />
+          Completed Bachelor's Degree at Ovidius University from Constanța,
+          Romania.
+        </BioSection>
+        <BioSection mb={4}>
+          <BioYear>
+            October <span style={{ color: '#81E6D9' }}> 2021 </span>
+          </BioYear>
+          <br />
+          Started Master's Degree at West University of Timișoara, Romania.
+        </BioSection>
+        <BioSection mb={4}>
+          <BioYear>
+            October <span style={{ color: '#81E6D9' }}> 2021 </span> to{' '}
+            <span style={{ color: '#81E6D9' }}> Present </span>{' '}
+          </BioYear>
+          <br />
+          Working at{' '}
+          <Link href="https://www.evonomix.com/" target="_blank">
+            EVONOMIX
+          </Link>
+          .
+        </BioSection>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I 💜
+        </Heading>
+        <Paragraph>
+          Since I was a kid I got my hand in many fields: football, basketball,
+          table tennis, boxing, dancing. All of these helped me gather a lot of
+          general culture, and form my vision upon everything around me.
+          Recently I started playing volleyball, and guitar lessons.
+          <br />
+          <br />A big passion of mine are computer games. Despite others
+          opinions, for me - video games are a way to relax after a long day, or
+          it's an alternative way to keep in touch with your friends that are
+          far away.
+          <br />
+          <br />
+          Here are some of my achievements:
+          <br />
+          <List>
+            <ListItem>
+              <Meta>CS:GO</Meta>Quarter finalist at Moldcell
+              Cup 2020
+            </ListItem>
+            <ListItem>
+              <Meta>CS:GO</Meta>
+              <Link
+                href="https://www.faceit.com/en/players/NiXL1337"
+                target="_blank"
+              >
+                Top 300
+              </Link>{' '}
+              players from region <em> (Republic of Moldova) </em>
+            </ListItem>
+            <ListItem>
+              <Meta>CS:GO</Meta>IGL and capitain of team <span style={{color: "#D657A5"}}>"CHGO"</span>
+            </ListItem>
+
+          </List>
+        </Paragraph>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          On the web
+        </Heading>
+
+        <List mb={3}>
+          <Center height="50px">
+            <Box>
+              <ListItem>
+                <Link href="https://github.com/N1ckSL" target="_blank">
+                  <Button
+                    variant="ghost"
+                    colorScheme="teal"
+                    leftIcon={<IoLogoGithub />}
+                  >
+                    GitHub
+                  </Button>
+                </Link>
+              </ListItem>
+            </Box>
+
+            <Box>
+              <ListItem>
+                <Link
+                  href="https://www.linkedin.com/in/nicolae-savilencu-7959861b6/"
+                  target="_blank"
+                >
+                  <Button
+                    variant="ghost"
+                    colorScheme="teal"
+                    leftIcon={<IoLogoLinkedin />}
+                  >
+                    LinkedIn
+                  </Button>
+                </Link>
+              </ListItem>
+            </Box>
+
+            <Box>
+              <ListItem>
+                <Link
+                  href="https://www.instagram.com/onfire_nick/"
+                  target="_blank"
+                >
+                  <Button
+                    variant="ghost"
+                    colorScheme="teal"
+                    leftIcon={<IoLogoInstagram />}
+                  >
+                    Instagram
+                  </Button>
+                </Link>
+              </ListItem>
+            </Box>
+
+            <ListItem>
+              <Link
+                href="https://steamcommunity.com/id/060034706/"
+                target="_blank"
+              >
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<IoLogoSteam />}
+                >
+                  Steam
+                </Button>
+              </Link>
+            </ListItem>
+            <ListItem>
+            <Tooltip
+                  label="NiXL#3224"
+                  aria-label="A tooltip"
+                  variant="ghost"
+                  colorScheme="teal"
+                >
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoDiscord />}
+              >
+              
+                  Discord
+              </Button>
+                </Tooltip>
+            </ListItem>
+          </Center>
+        </List>
+
+        <SimpleGrid my={5} columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            href="https://youtu.be/7OJucTAKXCE"
+            title="Single Page Social Network app"
+            thumbnail={thumbReact}
+          >
+            A Social Network created with <Badge colorScheme="blue"> React</Badge>
+          </GridItem>
+          <GridItem
+            href="https://mogogo.netlify.com/"
+            title="Mogo"
+            thumbnail={thumbMogo}
+            style={{minHeight: "200px"}}
+            
+          >
+             A modern <Badge colorScheme="orange"> landing page</Badge>
+          </GridItem>
+        </SimpleGrid>
+
+        <Box align="center" my={10}>
+          <NextLink href="/posts" scroll={false}>
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              Popular posts
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+    </Container>
+  </Layout>
+)
+
+export default Home
+export { getServerSideProps } from '../components/chakra'
---
+++ pages/posts.js
@@ -0,0 +1,83 @@
+import {
+  Badge,
+  Center,
+  Container,
+  Heading,
+  SimpleGrid,
+  Stack,
+  Text
+} from '@chakra-ui/react'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import { GridItem } from '../components/grid-item'
+
+
+const Posts = () => (
+  <Layout title="Posts">
+    <Container align="center">
+      <Heading as="h3" fontSize={20} mb={4}>
+        Popular Posts
+      </Heading>
+      <Center>
+        <Stack direction="row" mb={200} mt={10} style={{display:'flex', justifyContent:'center'}}>
+          <Text fontSize="xl"> Coming </Text>
+          <Badge colorScheme="cyan" fontSize="xl">
+            very
+          </Badge>
+          <Badge colorScheme="purple" fontSize="xl">
+            very
+          </Badge>
+          <Text fontSize="xl"> soon. </Text>
+        </Stack>
+      </Center>
+
+      {/* <Section delay={0.1}>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            title="My Fish workflow"
+            thumbnail={thumbFishWorkflow}
+            href="https://www.youtube.com/watch?v=KKxhf50FIPI"
+          />
+          <GridItem
+            title="My desk setup (Late 2020)"
+            thumbnail={thumbMyDeskSetup}
+            href="https://www.youtube.com/watch?v=1OFDMwDlnOE"
+          />
+        </SimpleGrid>
+      </Section>
+
+      <Section delay={0.3}>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            title="How I’ve Attracted The First 500 Paid Users For My SaaS That Costs $5/mo"
+            thumbnail={thumb500PaidUsers}
+            href="https://blog.inkdrop.app/how-ive-attracted-the-first-500-paid-users-for-my-saas-that-costs-5-mo-7a5b94b8e820"
+          />
+          <GridItem
+            title="I stopped setting a financial goal for my SaaS"
+            thumbnail={thumbFinancialGoal}
+            href="https://blog.inkdrop.app/i-stopped-setting-a-financial-goal-for-my-saas-a92c3db65506"
+          />
+        </SimpleGrid>
+      </Section>
+
+      <Section delay={0.5}>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            title="How to Price Yourself as a Freelance Developer"
+            thumbnail={thumbHowToPriceYourself}
+            href="https://blog.inkdrop.app/how-to-price-yourself-as-a-freelance-developer-3453dfd59d91"
+          />
+          <GridItem
+            title="I made my React Native app 50x faster"
+            thumbnail={thumb50xFaster}
+            href="https://www.youtube.com/watch?v=vj723NlrIQc"
+          />
+        </SimpleGrid>
+      </Section> */}
+    </Container>
+  </Layout>
+)
+
+export default Posts
+export { getServerSideProps } from '../components/chakra'
