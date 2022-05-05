623a40f4065489371049aa7906c5c514925e70ab
N1ckSL <savilencu.n@gmail.com>
2022-05-05 09:54:05 +0300

    disable EVX project (copyright)

--- pages/works.js
+++ pages/works.js
@@ -11,7 +11,7 @@ import { WorkGridItem } from '../components/grid-item'
 
 import thumbMogo from '../public/images/works/mogo.png'
 import thumbNade from '../public/images/works/nade.png'
-import thumbEvx from '../public/images/works/evx.png'
+// import thumbEvx from '../public/images/works/evx.png'
 import thumbReact from '../public/images/works/react.png'
 import thumbMern from '../public/images/works/mock.png'
 import thumbPortfolio from '../public/images/works/portfolio.png'
@@ -60,7 +60,7 @@ const Works = () => (
           Old Works
         </Heading>
 
-        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        {/* <SimpleGrid columns={[1, 1, 2]} gap={6}>
           <Section>
             <WorkGridItem id="evx" title="Travel agency" thumbnail={thumbEvx}>
               <Badge colorScheme="green">Completed</Badge>
@@ -71,7 +71,8 @@ const Works = () => (
               <Badge colorScheme="red">postponed</Badge>
             </WorkGridItem>
           </Section>
-        </SimpleGrid>
+        </SimpleGrid> */}
+
       </Container>
     </Container>
   </Layout>
